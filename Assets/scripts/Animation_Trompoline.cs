8d79ede9a4727bcb716163bc7451eb18e36203b4
DarwinArath <darwin.koyoctun@gmail.com>
2021-06-13 03:49:37 -0500

    Trompolin

---
+++ Assets/scripts/Animation_Trompoline.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Trompoline : MonoBehaviour
+{
+    public Animator anim;
+
+    public float jumpForce = 2f;
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D col){
+        if (col.transform.CompareTag("Player")){
+            col.gameObject.GetComponent<Rigidbody2D>().velocity = (Vector2.up * jumpForce);
+            anim.Play("Animation_Trompoline");
+        }
+    } 
+}
